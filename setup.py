a8f6bb858a51f8853864bddb8d897092422b5713
letwir <letwirwinkuru@gmail.com>
2026-05-12 11:26:57 +0900

    fix author

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@
 
 import setuptools
 
+
 def slurp(fn):
     with open(fn, "r", encoding="utf-8") as fh:
         return fh.read()
@@ -16,26 +17,22 @@ def slurp(fn):
 setuptools.setup(
     name="trackfs",
     version=slurp("VERSION"),
-    authors="Andreas Schmidt"
+    authors="Andreas Schmidt, letwir",
     author_email="author@example.com",
     description="A read-only FUSE filesystem that splits FLAC+CUE files into individual FLAC files per track",
     long_description=slurp("README.py.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/letwir/trackfs",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "mutagen", "fuse", "Lark", "chardet", "psutils"
-    ],
+    install_requires=["mutagen", "fuse", "Lark", "chardet", "psutils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: POSIX",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
     entry_points={
-        'console_scripts': [
-            'trackfs=trackfs.__init__:main'
-        ],
-    }
+        "console_scripts": ["trackfs=trackfs.__init__:main"],
+    },
 )
